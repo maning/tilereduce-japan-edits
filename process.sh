e297c6e626630ca32ada27efb33520a3e7809f61
maning <emmanuel.sambale@gmail.com>
2015-11-26 18:03:08 +0530

    added sh script to automate the process and japan_poly.gejson for the mbtiles-extracts

---
+++ process.sh
@@ -0,0 +1,29 @@
+#! /bin/sh -x
+
+user= #mapbox user to uplod the mbtiles
+token=  #MapboxAccessToken
+poly=japan_poly.geojson  #clipping polygon
+out=output.gejson #geojeson output from tile-reduce process
+
+mkdir tmp
+
+# Get the latest OSM-QA tiles
+wget https://s3.amazonaws.com/mapbox/osm-qa-tiles/latest.planet.mbtiles.gz
+
+# clip mbtiles to country polugon
+gunzip -c latest.planet.mbtiles.gz  > latest.planet.mbtiles
+mbtiles-extracts latest.planet.mbtiles ../${clipping_poly} name
+
+# extract all highways for a given timestamp 
+node index.js > ${out}
+
+# Convert to mbtiles
+tippecanoe -f -o japan_highway_edits.mbtiles -l jp_highway_edits -n "Japan Highways after 20150902 z5-18" -z18 -Z5 -g1 ${out}
+
+# Upload to mapbox
+
+export MapboxAccessToken=$token 
+mapbox-upload $user.japan_highways_edits japan_highway_edits.mbtiles
+
+rm -rf ../tmp/*
+
